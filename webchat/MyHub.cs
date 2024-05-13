f53508c4370432aa4aff280970494496d672e25f
ilanae03 <ilan.eschiezaroarnaut@gmail.com>
2024-05-13 20:25:06 -0300

    primeiro commit

---
+++ webchat/MyHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace webchat
+{
+    public class MyHub : Hub
+    {
+        public async Task EnviarMensagem(string mensagem)
+        {
+            await Clients.All.SendAsync("ReceberMensagem", mensagem);
+        }
+    }
+}
