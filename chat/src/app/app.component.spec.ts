7f2235e74558d1b381cf2ec65a7a61c2454c80b7
ilanae03 <ilan.eschiezaroarnaut@gmail.com>
2024-05-13 20:30:45 -0300

    commit do chat

--- chat/src/app/app.component.spec.ts
+++ chat/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'chat' title`, () => {
+  it(`should have the 'app-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('chat');
+    expect(app.title).toEqual('app-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, chat');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-angular');
   });
 });
